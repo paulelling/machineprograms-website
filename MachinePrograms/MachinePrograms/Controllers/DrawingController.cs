733f49f61ccc9c4775830c5ea651eacbfa2bd34d
paulelling <paulelling@yahoo.com>
2025-05-31 03:08:48 -0500

    Added drawing page

---
+++ MachinePrograms/MachinePrograms/Controllers/DrawingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MachinePrograms.Controllers
+{
+    public class DrawingController : Controller
+    {
+        public IActionResult Sketches()
+        {
+            return View();
+        }
+    }
+}
